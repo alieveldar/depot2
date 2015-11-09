422b72490330055a548b18266e439648d7fc97eb
modus <illuminatiday@gmail.com>
2015-11-09 20:01:08 +0300

    midific product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+validates :title, :description, :image_url, presence: true
+validates :price, numericality: {greater_than_or_equal_to: 0.1}
+validates :title, uniqueness: true
+validates :image_url, allow_blank: true, format: {
+ with: %r{\.(gif|jpg|jpeg|tif|png)$}i, 
+ message: 'Адрес должен быть указан на изображение в формате гиф джипег и тдэ'
+ }
 end
