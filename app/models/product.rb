daf61d7876925cef6c4e5a78fcaa6ada8475beaa
modus <illuminatiday@gmail.com>
2015-11-22 13:33:26 +0300

    #001 Correct Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.1}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: {
- with: %r{\.(gif|jpg|jpeg|tif|png)/A}i, 
+ with: /[.](gif|jpg|jpeg|tif|png)\z/i, 
  message: 'Адрес должен быть указан на изображение в формате гиф джипег и тдэ'
  }
 end
