39ac0873a72f93fd804a674c0cac4cd067bf737f
modus <illuminatiday@gmail.com>
2015-11-23 22:08:29 +0300

    #007 Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,17 @@ validates :image_url, allow_blank: true, format: {
  with: /[.](gif|jpg|jpeg|tif|png)\z/i, 
  message: 'Адрес должен быть указан на изображение в формате гиф джипег и тдэ'
  }
+ has_many :lime_items
+ before_destroy :ensure_not_referenced_by_any_line_item
+ private
+ # убеждаемся в оствутствии товарных похзиций, ссылающихся на данный товар
+ def ensure_not_referbced_by_any_line_item
+ 	if line_items.empty?
+ 		return true
+ 	else
+ 		errors.add(:base, 'существуют товарные позиции')
+ 		return false
+ 	end
+ end
+ 
 end
